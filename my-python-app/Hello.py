529dcc254be5d743ee215dfba9e6886a6739566e
yonatasoftware <mail2tapasdas@gmail.com>
2025-06-28 13:00:38 +0530

    Method signature changed

--- my-python-app/Hello.py
+++ my-python-app/Hello.py
@@ -1,11 +1,11 @@
 # hello.py
 
-def greet(name):
+def greet(name,age):
     """Return a greeting for a given name."""
-    return f"Hello, {name}!"
+    return f"Hello, {name}! your age is {age}"
 
 if __name__ == "__main__":
     user = input("Enter your name: ")
     age = input("Enter your age ")
-    print(greet(user))
+    print(greet(user,age))
     print(age)
